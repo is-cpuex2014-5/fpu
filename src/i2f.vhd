e9801c4c82878557c9ff2405fc69a243ddd63266
Masaki Waga <masakiwaga+github@gmail.com>
2014-12-17 13:25:52 +0900

    Revert "i2fをnumeric_stdで書きなおした"
    This reverts commit 24a97033c4d0ea42bfa5c818cd6367b7e142f2c6.

--- src/i2f.vhd
+++ src/i2f.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
 use IEEE.numeric_std.all;
 
 entity i2f is 
@@ -13,7 +14,7 @@ architecture behav of i2f is
   component ZLC31 is    
     port (
       A : in  std_logic_vector (30 downto 0);
-      Q : out integer range 0 to 31);
+      Q : out std_logic_vector (4 downto 0));
   end component ZLC31;
 
   signal isZero : boolean := false;
@@ -22,57 +23,54 @@ architecture behav of i2f is
   signal mantissa : std_logic_vector (22 downto 0) := (others => '0');
   signal i : std_logic_vector (30 downto 0) := (others => '0');
   signal raw_mantissa : std_logic_vector (30 downto 0) := (others => '0');
-  signal s : integer range 0 to 31 := 0;
+  signal s : std_logic_vector (4 downto 0) := (others => '0');
   signal R : std_logic := '0';
   signal G : std_logic := '0';
   signal ulp : std_logic := '0';
-  signal round : integer range 0 to 1 := 0;
+  signal round : std_logic := '0';
 begin  -- architecture behav
   with sign select
     raw_mantissa <=
-    std_logic_vector(to_unsigned(to_integer(unsigned(not i)) + 1,31)) when '1',
+    (not i) + 1 when '1',
     i when others;
 
   ZLC:ZLC31 port map(raw_mantissa,s);
 
   with s < 31 select
     expr <=
-    std_logic_vector(to_unsigned(157 - s,8)) when true,
-    (others => '0')                          when others;
+    "00000000" + 157 - s when true,
+    "00000000"           when others;
 
   with s < 7 select
     R <=
-    raw_mantissa (6- s) when true,
+    raw_mantissa (conv_integer(6-s)) when true,
     '0'                              when others;
 
-  with s < 6 and to_integer(unsigned(raw_mantissa (5-s downto 0))) /= 0 select
+  with s < 6 and conv_integer(raw_mantissa (conv_integer(5-s) downto 0)) /= 0 select
     G <=
     '1'                when true,
     '0'                when others;
 
   with s < 8 select
     ulp <=
-    raw_mantissa (7-s) when true,
+    raw_mantissa (conv_integer(7-s)) when true,
     '0'                              when others;
 
-  with (R and (G or ulp)) select
-    round <=
-    1 when '1',
-    0 when others;
+  round <= R and (G or ulp);
 
   with s < 7 select
     mantissa <=
     std_logic_vector(
       shift_left (arg => unsigned(raw_mantissa),
-                  count => s-7)(22 downto 0)) when false,
+                  count => conv_integer(s-7))(22 downto 0)) when false,
     std_logic_vector(
       shift_right (arg => unsigned(raw_mantissa),
-                   count => 7-s)(22 downto 0)) when others;
+                   count => conv_integer(7-s))(22 downto 0)) when others;
 
   with isZero select
     Q <=
-    sign & std_logic_vector(to_unsigned(to_integer(unsigned(expr & mantissa)) + round,31)) when false,
-    (others => '0')                  when others;
+    (sign & expr & mantissa) + round when false,
+    x"00000000"                      when others;
 
   -- purpose: set input
   -- type   : combinational
